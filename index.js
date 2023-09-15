73a3043e9c138e8d0d5052c4848a60b2f88c1d57
JavaScythe <slashmod6@gmail.com>
2023-09-15 09:04:36 -0400

    movement

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ let state = {
     isOwned: false,
     isStarted: false,
     conf: undefined,
-    wss: []
+    wss: [],
+    map: undefined,
+    playerExacts: {
+        "p0": {
+            x: 0,
+            y: 0
+        },
+        "p1": {
+            x: 12,
+            y: 0
+        },
+        "p2": {
+            x: 0,
+            y: 10
+        },
+        "p3": {
+            x: 12,
+            y: 10
+        }
+    },
+    playerKeyStates: []
 }
 const server = Bun.serve({
     fetch(req, server) {
@@ -35,6 +55,8 @@ const server = Bun.serve({
             } else if(message.reg == true){
                 state.wss.push(ws);
                 return;
+            } else if (message.type == "keyu"){
+                think(message.key, ws.player);
             }
             if(ws.owner == true && state.isStarted == false){
                 state.conf = message;
@@ -46,6 +68,11 @@ const server = Bun.serve({
                         type: "map",
                         map: state.map
                     }));
+                    state.wss[i].send(JSON.stringify({
+                        type: "things",
+                        things: state.things
+                    }));
+                    state.wss[i].player = "p"+i;
                 }
             }
         },
@@ -55,15 +82,38 @@ const server = Bun.serve({
     },
     port: 3001
 });
+function think(k,p){
+    if(k == "ArrowRight"){
+        state.playerExacts[p].x+=0.1;
+        console.log("updated");
+        broadcast({
+            "type":"px",
+            "player": p,
+            "x": state.playerExacts[p].x,
+            "y": state.playerExacts[p].y
+        })
+    }
+}
+function broadcast(m){
+    console.log(m);
+    for(let i in state.wss){
+        state.wss[i].send(JSON.stringify(m));
+    }
+}
 function mapGen(x){
+    const mapConst = ["", "X"];
     if(x == "default"){
         let map = [];
         for(let y=0;y<11;y++){
             map.push([]);
             for(let x=0;x<13;x++){
-                map[y].push(Math.round(Math.random()));
+                map[y].push(mapConst[Math.floor(Math.random()*mapConst.length)]);
             }
         }
+        map[0][0] = "p0";
+        map[0][12] = "p1";
+        map[10][0] = "p2";
+        map[10][12] = "p3";
         return map;
     } else {
         throw "b0zo";
