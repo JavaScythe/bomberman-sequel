747b584380aab140b9d060d4897e396fa9160a52
JavaScythe <slashmod6@gmail.com>
2023-09-14 23:11:29 -0400

    stuff mk35

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let state = {
     isOwned: false,
     isStarted: false,
-    conf: undefined
+    conf: undefined,
+    wss: []
 }
 const server = Bun.serve({
     fetch(req, server) {
@@ -25,13 +26,27 @@ const server = Bun.serve({
             } catch(e){
                 console.log("malformed send");
             }
-            if(ws.reg == true && !isOwned){
+            if(message.reg == true && state.isOwned == false){
                 ws.owner = true;
-                isOwned=true;
+                console.log("everyday");
+                state.isOwned=true;
+                state.wss.push(ws);
+                return;
+            } else if(message.reg == true){
+                state.wss.push(ws);
                 return;
             }
-            if(ws.owner == true && isStarted == false){
+            if(ws.owner == true && state.isStarted == false){
                 state.conf = message;
+                state.isStarted = true;
+                console.log("LESGO");
+                state.map = mapGen(state.conf.map);
+                for(let i in state.wss){
+                    state.wss[i].send(JSON.stringify({
+                        type: "map",
+                        map: state.map
+                    }));
+                }
             }
         },
         close(ws) {
@@ -40,4 +55,18 @@ const server = Bun.serve({
     },
     port: 3001
 });
+function mapGen(x){
+    if(x == "default"){
+        let map = [];
+        for(let y=0;y<11;y++){
+            map.push([]);
+            for(let x=0;x<13;x++){
+                map[y].push(Math.round(Math.random()));
+            }
+        }
+        return map;
+    } else {
+        throw "b0zo";
+    }
+}
 console.log(`Listening on http://localhost:${server.port}`);
